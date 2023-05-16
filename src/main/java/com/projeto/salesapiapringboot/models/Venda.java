d527b4940c65dccd9f9dd0d09e93c6b11339e665
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-05-16 12:50:41 -0300

    Add ORM annotations to Venda and Vendedor models

--- src/main/java/com/projeto/salesapiapringboot/models/Venda.java
+++ src/main/java/com/projeto/salesapiapringboot/models/Venda.java
@@ -3,11 +3,30 @@ package com.projeto.salesapiapringboot.models;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_venda")
 public class Venda {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "data_venda")
 	private LocalDate dataVenda;
+	
 	private Double valor;
+	
+	@ManyToOne
+	@JoinColumn(name = "vendedor_id")
 	private Vendedor vendedor;
 	
 	public Venda() {
--- src/main/java/com/projeto/salesapiapringboot/models/Vendedor.java
+++ src/main/java/com/projeto/salesapiapringboot/models/Vendedor.java
@@ -2,9 +2,22 @@ package com.projeto.salesapiapringboot.models;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_vendedor")
 public class Vendedor {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "vendedor_nome")
 	private String nome;
 	
 	public Vendedor() {
